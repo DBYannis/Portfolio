b0ca0d4521d9733dff36ff873529a4838eb6ae68
DBYannis <yannis.denantboemont@hotmail.com>
2022-09-20 16:28:45 +0200

    git commit -m "added music player"

---
+++ data.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: "YOU'RE THE ONE",
+    path: 'music/kaytra.mp3',
+    artist: 'KAYTRANADA',
+    cover: 'images/kaytra.jpg'
+  },
+  {
+    name: "The Less I Know The Better",
+    path: 'music/impala.mp3',
+    artist: 'Tame Impala',
+    cover: 'images/impala.jpg'
+  },
+  {
+    name: "Redbone",
+    path: 'music/redbone.mp3',
+    artist: 'Childish Gambino',
+    cover: 'images/redbone.jpg'
+  },
+  {
+    name: "SEE YOU AGAIN",
+    path: 'music/tyler.mp3',
+    artist: 'Tyler, The Creator',
+    cover: 'images/tyler.jpg'
+  },
+]
